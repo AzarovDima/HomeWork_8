9bc98de30586560da09e329155a54056900ea035
AzarovDima <ada89885308140@gmail.com>
2023-01-30 23:49:48 +0300

    Начал решать произведение матриц

--- Challenge_3_56/Program.cs
+++ Challenge_3_56/Program.cs
@@ -1,3 +1,37 @@
 ﻿//Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 
 
+
+int[,] matrix1 = GetMatrix(3, 3, 1, 10);
+int[,] matrix2 = GetMatrix(5, 5, 1, 10);
+
+Console.WriteLine("Дана пернвая матрица:");
+PrintMatrix(matrix1);
+Console.WriteLine();Console.WriteLine("Дана вторая матрица:");
+PrintMatrix(matrix2);
+
+
+int[,] GetMatrix(int m, int n, int minValue, int maxValue)
+{
+    int[,] newMatrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            var rnd = new Random();
+            newMatrix[i, j] = rnd.Next(minValue, maxValue);
+        }
+    }
+    return newMatrix;
+}
+void PrintMatrix(int[,] inMatrix)
+{
+    for (int i = 0; i < inMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inMatrix.GetLength(1); j++)
+        {
+            Console.Write($"{inMatrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
