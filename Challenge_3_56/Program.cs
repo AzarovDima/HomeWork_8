3e51f52e3b01d6f16c5c2cf2c25bdfb35277b178
AzarovDima <ada89885308140@gmail.com>
2023-01-31 21:09:03 +0300

    Произведение матриц готово

--- Challenge_3_56/Program.cs
+++ Challenge_3_56/Program.cs
@@ -1,15 +1,18 @@
 ﻿//Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 
 
-
-int[,] matrix1 = GetMatrix(3, 3, 1, 10);
-int[,] matrix2 = GetMatrix(5, 5, 1, 10);
+// формула умножения двух матриц!!! Где матрица 1 имеет размер M х K, матрица 2 — K х N, и матрица 3 — M х N. 
+int[,] matrix1 = GetMatrix(4, 5, 1, 10);
+int[,] matrix2 = GetMatrix(7, 3, 1, 10);
+int[,] resultMatrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)]; // размер новой матрицы после умножения 
+MultiplyMatrix(matrix1, matrix2, resultMatrix);                            
 
 Console.WriteLine("Дана пернвая матрица:");
 PrintMatrix(matrix1);
-Console.WriteLine();Console.WriteLine("Дана вторая матрица:");
+Console.WriteLine("Дана вторая матрица:");
 PrintMatrix(matrix2);
-
+Console.WriteLine("Произведение двух матриц:");
+PrintMatrix(resultMatrix);
 
 int[,] GetMatrix(int m, int n, int minValue, int maxValue)
 {
@@ -33,5 +36,20 @@ void PrintMatrix(int[,] inMatrix)
             Console.Write($"{inMatrix[i, j]} ");
         }
         Console.WriteLine();
+    }
+}
+void MultiplyMatrix(int[,] matrix1, int[,] matrix2, int[,] resultMatrix)
+{
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < matrix1.GetLength(1); k++)
+            {
+                sum += matrix1[i, k] * matrix2[k, j];
+            }
+            resultMatrix[i, j] = sum;
+        }
     }
 }
